a139f596cd372dde17d1c8a861fd9425b64686c2
Jesse Scott <jesse@memelab.ca>
2013-07-13 16:07:58 -0700

    SIngle View Application

---
+++ ViewBook/ViewBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewBook
+//
+//  Created by Jesse Scott on 2013-07-13.
+//  Copyright (c) 2013 Emily Carr Institute of Art + Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewBook/ViewBook/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ViewBook
+//
+//  Created by Jesse Scott on 2013-07-13.
+//  Copyright (c) 2013 Emily Carr Institute of Art + Design. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

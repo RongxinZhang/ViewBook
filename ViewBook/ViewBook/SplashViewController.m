0dcac98f16c021fd6a9d414de41ca938f1def037
Jesse Scott <jesse@memelab.ca>
2013-07-13 16:16:17 -0700

    SplashViewController
    with logo and renamed class

--- ViewBook/ViewBook/SplashViewController.m
+++ ViewBook/ViewBook/SplashViewController.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Emily Carr Institute of Art + Design. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "SplashViewController.h"
 
 @interface ViewController ()
 

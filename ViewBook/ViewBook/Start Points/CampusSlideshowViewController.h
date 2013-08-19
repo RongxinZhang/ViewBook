a2874224265d0d8b92fa5bc6efea17956a056f53
Jesse Scott <jesse@memelab.ca>
2013-08-19 00:23:40 -0700

    Animated UIImage Slideshow for Studios & Campus
    (Studios isnt showing up - jpg issue ??)

---
+++ ViewBook/ViewBook/Start Points/CampusSlideshowViewController.h
@@ -0,0 +1,20 @@
+//
+//  CampusSlideshowViewController.h
+//  ViewBook
+//
+//  Created by Jesse Scott on 2013-08-19.
+//  Copyright (c) 2013 Emily Carr Institute of Art + Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CampusSlideshowViewController : UIViewController {
+    
+    
+}
+
+// Image View
+@property (weak, nonatomic) IBOutlet UIImageView *slideshow;
+
+
+@end

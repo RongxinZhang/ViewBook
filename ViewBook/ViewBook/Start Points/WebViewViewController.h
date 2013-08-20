79f9052ff9dc02da7379e5cfed5f6a6f4776df5d
Jesse Scott <jesse@memelab.ca>
2013-08-19 23:51:53 -0700

    Vimeo Embedding !!!

---
+++ ViewBook/ViewBook/Start Points/WebViewViewController.h
@@ -0,0 +1,20 @@
+//
+//  WebViewViewController.h
+//  ViewBook
+//
+//  Created by Jesse Scott on 2013-08-19.
+//  Copyright (c) 2013 Emily Carr Institute of Art + Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewViewController : UIViewController {
+    
+    
+}
+
+// Web View
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+
+@end

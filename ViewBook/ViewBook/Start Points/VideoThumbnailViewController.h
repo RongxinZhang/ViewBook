7e274adb6178aa681ffbce9b1a0a1e6873653142
Jesse Scott <jesse@memelab.ca>
2013-08-25 23:54:48 -0700

    Passign Vimeo Id To New Screen

---
+++ ViewBook/ViewBook/Start Points/VideoThumbnailViewController.h
@@ -0,0 +1,29 @@
+//
+//  VideoThumbnailViewController.h
+//  ViewBook
+//
+//  Created by Jesse Scott on 2013-08-25.
+//  Copyright (c) 2013 Emily Carr Institute of Art + Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoThumbnailViewController : UIViewController {
+    
+    NSString *vimeoURL;
+    
+}
+
+- (IBAction)launchCreativeFuture:(id)sender;
+
+- (IBAction)launchDayInTheLife:(id)sender;
+
+- (IBAction)launchDesignCommunity:(id)sender;
+
+- (IBAction)launchErinBoniferro:(id)sender;
+
+- (IBAction)launchGiantAnt:(id)sender;
+
+- (IBAction)launchLukeParnell:(id)sender;
+
+@end
--- ViewBook/ViewBook/Start Points/WebViewViewController.m
+++ ViewBook/ViewBook/Start Points/WebViewViewController.m
@@ -15,7 +15,7 @@
 @implementation WebViewViewController
 
 // Synthesize Variables
-@synthesize webView;
+@synthesize webView, vimeoURL;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -25,9 +25,17 @@
     return self;
 }
 
+-(id)initWithURL:(NSString*)url {
+    vimeoURL = [[NSString alloc] initWithString:url];
+    NSLog(@"The URL received is %@", vimeoURL);
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    NSLog(@"VDL");
     
     // Embed the Video
     [self embedVimeo];
@@ -37,8 +45,12 @@
 
 -(void)embedVimeo {
     
-    NSString *embedHTML = @"<iframe width=\"300\" height=\"250\" src=\"http://player.vimeo.com/video/65436359\" frameborder=\"0\" allowfullscreen></iframe>";
-        
+    NSString *pre = @"<iframe width=\"300\" height=\"250\" src=\"http://player.vimeo.com/video/";
+    NSString *post = @"\" frameborder=\"0\" allowfullscreen></iframe>";
+    
+    NSString *embedHTML = [NSString stringWithFormat:@"%@%@%@", pre, vimeoURL, post];
+    NSLog(@"%@", embedHTML);
+    
     [webView loadHTMLString:embedHTML baseURL:nil];
     //[self.view addSubview:webView];
 }

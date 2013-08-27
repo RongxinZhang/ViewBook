1647d0de223271528d7f73c540c9376ad7be54a8
Jesse Scott <jesse@memelab.ca>
2013-08-26 19:33:58 -0700

    not sure

--- ViewBook/ViewBook/Start Points/WebViewViewController.m
+++ ViewBook/ViewBook/Start Points/WebViewViewController.m
@@ -16,7 +16,7 @@
 @implementation WebViewViewController
 
 // Synthesize Variables
-@synthesize webView, vimeoURL;
+@synthesize webView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -26,6 +26,7 @@
     return self;
 }
 
+<<<<<<< HEAD
 -(id)initWithURL:(NSString*)url {
     vimeoURL = [[NSString alloc] initWithString:url];
     NSLog(@"The URL received is %@", vimeoURL);
@@ -33,11 +34,11 @@
 }
 
 
+=======
+>>>>>>> parent of 7e274ad... Passign Vimeo Id To New Screen
 - (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    
-    NSLog(@"VDL");
     
     // Embed the Video
     [self embedVimeo];
@@ -57,12 +58,8 @@
 
 -(void)embedVimeo {
     
-    NSString *pre = @"<iframe width=\"300\" height=\"250\" src=\"http://player.vimeo.com/video/";
-    NSString *post = @"\" frameborder=\"0\" allowfullscreen></iframe>";
-    
-    NSString *embedHTML = [NSString stringWithFormat:@"%@%@%@", pre, vimeoURL, post];
-    NSLog(@"%@", embedHTML);
-    
+    NSString *embedHTML = @"<iframe width=\"300\" height=\"250\" src=\"http://player.vimeo.com/video/65436359\" frameborder=\"0\" allowfullscreen></iframe>";
+        
     [webView loadHTMLString:embedHTML baseURL:nil];
     //[self.view addSubview:webView];
 }
